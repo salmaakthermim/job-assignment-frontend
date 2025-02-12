ba2beb666120ff0d1b03f51d27096a903b50467e
salmaakthermim <salmaakthermim112@gmail.com>
2025-02-12 16:43:30 +0600

    assignment update

--- src/App.jsx
+++ src/App.jsx
@@ -11,9 +11,7 @@ function App() {
      
       
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+      
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
