1e0a31edce9ebaaaadfc0efac0af6364a66dc689
salmaakthermim <salmaakthermim112@gmail.com>
2025-02-12 16:44:35 +0600

    online update

--- src/App.jsx
+++ src/App.jsx
@@ -12,9 +12,7 @@ function App() {
       
       <div className="card">
       
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
